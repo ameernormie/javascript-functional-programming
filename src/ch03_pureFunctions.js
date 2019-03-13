1fa9b0c492cd0bae1c73a6aa1fa928b8ecc5cad9
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-03-13 14:20:45 +0500

    intro to pure functions

---
+++ src/ch03_pureFunctions.js
@@ -0,0 +1,26 @@
+// Cacheable
+
+const memoize = f => {
+  const cache = {};
+
+  return (...args) => {
+    const argStr = JSON.stringify(args);
+    cache[argStr] = cache[argStr] || f(...args);
+    return cache[argStr];
+  };
+};
+
+const squareNumber = memoize(x => x * x);
+
+squareNumber(4); // 16
+
+squareNumber(4); // 16, returns cache for input 4
+
+squareNumber(5); // 25
+
+squareNumber(5); // 25, returns cache for input 5
+
+//*************//
+// Referencial Transparency
+
+// Parallel  code
